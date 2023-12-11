b6b531b5f693b2e5a153e9b88831f9f8c18894a8
2020b-Pethő Boglárka <74898888+b0petbog@users.noreply.github.com>
2023-12-11 08:03:15 +0100

    Add files via upload

---
+++ Models/Hamster.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace oop.Model
+{
+    public class Hamster
+    {
+        private double number;
+        private double time;
+        private double consuption;
+        public Hamster(double number, double time, double consuption)
+        {
+            this.number = number;
+            this.time = time;
+            this.consuption = consuption;
+        }
+
+        public double Number
+        {
+            get => number; set => number = value;
+        }
+
+        public double Time
+        {
+            get => time; set => time = value;
+        }
+
+        public double Consuption
+        {
+            get => consuption; set => consuption = value;
+        }
+        //Egy hörcsög egy óra kerekezéssel 28 watt energiát állít elő
+        public double EnergyOneDay
+        {
+            get
+            {
+                double oneday = number*time*28;
+                return oneday;
+            }
+        }
+
+        public double EnergyOneMonth
+        {
+            get
+            {
+                double onemonth = EnergyOneDay * 30;
+                return onemonth;
+            }
+        }
+
+        public double EnergyPercent
+        {
+            get 
+            {
+                double energy = Math.Round(EnergyOneMonth/consuption*100);
+                return energy;
+            }
+        }
+
+        public double NumberOfHamsters
+        {
+            get
+            {
+                //egy hörcsögre jutó termelt energia egy hónapban a megadott kerekezési idővel
+                double oneHamster = time*28*30;
+                double hamsterNumber = consuption/oneHamster;
+                return hamsterNumber;
+            }
+        }
+    }
+}
---
+++ ViewModels/HamsterViewModel.cs
@@ -0,0 +1,106 @@
+﻿using oop.Model;
+using System;
+using System.Windows;
+using ViewModels.BaseClass;
+
+namespace HamsterBatteryProject.ViewModels
+{
+        public class HamsterViewModel : ViewModelBase
+        {
+            private Hamster _hamster;
+
+            public HamsterViewModel()
+            {
+                _hamster = new Hamster(1, 5, 200000);
+                CalculationCommand = new RelayCommand(execute => Calculation());
+            }
+            public RelayCommand CalculationCommand { get; private set; }
+
+            public double Number
+            {
+                get
+                {
+                    return _hamster.Number;
+                }
+                set
+                {
+                    double number = Convert.ToDouble(value);
+                    _hamster.Number = number;
+                }
+            }
+            public double Time
+            {
+                get
+                {
+                    return _hamster.Time;
+                }
+
+                set
+                {
+                    double time = Convert.ToDouble(value);
+                    _hamster.Time = time;
+                }
+            }
+
+            public double Consuption
+            {
+                get
+                {
+                    return _hamster.Consuption;
+                }
+
+                set
+                {
+                    double consuption = Convert.ToDouble(value);
+                    _hamster.Consuption = consuption;
+                }
+            }
+
+            public string EnergyOneDay
+            {
+                get
+                {
+                    double number = _hamster.Number;
+                    double energy= _hamster.EnergyOneDay;
+                    return number + " darab hörcsög egy nap alatt " + energy + " W energiát termel";
+                }
+            }
+
+            public string EnergyOneMonth
+            {
+                get
+                {
+                    double number = _hamster.Number;
+                    double energy = _hamster.EnergyOneMonth;
+                    return number + " darab hörcsög egy hónap alatt " + energy + " W energiát termel";
+                }
+            }
+
+            public string EnergyPercent
+            {
+                get
+                {
+                    double energypercent = _hamster.EnergyPercent;
+                    return "A megtermelt energia a háztartás energia használtának " +energypercent+ "%-a";
+                }
+            }
+
+            public string NumberOfHamsters
+            {
+                get
+                {
+                    double hamsterNumber = Math.Ceiling(_hamster.NumberOfHamsters);
+                    double fullenergy = _hamster.Consuption;
+                    return fullenergy +" W energia megtermeléséhez "+hamsterNumber+" db hörcsög szükséges";
+                }
+            }
+
+            private void Calculation()
+            {
+                OnPropertyChanged(nameof(EnergyOneDay));
+                OnPropertyChanged(nameof(EnergyOneMonth));
+                OnPropertyChanged(nameof(EnergyPercent));
+                OnPropertyChanged(nameof(NumberOfHamsters));
+            }
+        }
+}
